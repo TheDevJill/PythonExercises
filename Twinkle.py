472fc35b77a6e4e8563ee235a7a810204f63b242
TheDevJill <104874646+TheDevJill@users.noreply.github.com>
2022-06-06 21:03:14 -0700

    Update Twinkle.py

--- Twinkle.py
+++ Twinkle.py
@@ -1,2 +1,2 @@
 # ---!w3resource: Python Basics: ex. 1
-print("Twinkle. twinkle, little star, n\t How I wonder what you are! n\t\t Up above the world so high, n\t\t Like a diamond in the sky. n\ Twinkkle, twinkle, little star, n\t How I wonder what you are!
+print("Twinkle. twinkle, little star, n\t How I wonder what you are! n\t\t Up above the world so high, n\t\t Like a diamond in the sky. n\ Twinkkle, twinkle, little star, n\t How I wonder what you are!)
